010ec68201b8efdc301524bc11d840a428f894bc
samannrz <saman.noorzadeh@surgar-surgery.com>
2022-08-17 11:41:03 +0200

    "Testing"

---
+++ main.py
@@ -0,0 +1,12 @@
+from functions import *
+# This codes takes some statistics on the Workspaces
+# and save the dataframe to google sheet
+
+teamName = 'Endometriosis'
+workspaceName = 'Data annotation'
+sheetName = teamName + '/' + workspaceName
+
+sfpath = 'C:/Users/SurgAR-User/OneDrive/Documents/Saman/mypythonfolder/supervisely-python-sdk-example/my-gpysheets-3d8d13442005.json'
+sheetID = '1R_2NpRg40U08F2gl4dM0OzXcN4f0IQaW3yfXZmBWqCg'
+datadf = pdStats (teamName,workspaceName)
+write_to_gsheet(sfpath, sheetID, sheetName, datadf)
